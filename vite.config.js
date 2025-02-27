50ecfae55dcefe18f3b604e94291b9c8a60ba036
Jhonabel <l.jhonabel.06@gmail.com>
2025-02-27 17:59:42 -0400

    Upgrade de VITE

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    host: true, // Permite acceso desde cualquier IP en la red
+    //port: 5173, // Opcional: cambia el puerto si es necesario
+  }
 
+})
